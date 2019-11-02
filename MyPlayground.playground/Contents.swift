c9083c13a45807d7a425f2f3eb47979ee1ebfd73
100seungho <pouy1015@snu.ac.kr>
2019-11-02 23:30:41 +0900

    Egg Timer (handling Timer)

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,6 +1,17 @@
+var dict = ["Amy":1, "James":2, "Kyle":3]
+dict["Amy"]
 
-func greeting() {
-    print("Hello")
+var studentsAndScores = ["Amy": Int(readLine()!)!, "James": Int(readLine()!)!, "Helen": Int(readLine()!)!]
+
+func highestScore(scores: [String : Int]) {
+    var max: Int = 0
+  //Write your code here.
+    for i in scores.values{
+        if i > max { max = i }
+    }
+  
+    print(max)
 }
 
-greeting()
+//Don't change this
+highestScore(scores: studentsAndScores)
